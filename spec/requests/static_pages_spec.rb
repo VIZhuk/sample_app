ebd32ee71fb1025fef3c7726bfa001790f61f02f
Vladislav Zhukovitskij <v.zhukovitskij@gmail.com>
2015-02-04 06:42:29 +0200

    Add a full_title helper

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+#describe "StaticPages" do
+#  describe "GET /static_pages" do
+#    it "works! (now write some real specs)" do
+#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#      get static_pages_index_path
+#      response.status.should be(200)
+#    end
+#  end
+#end
+
+describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "Home Page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("| Home")
+    end
+  end
+
+  describe "Help Page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
+  describe "About Page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
+end
