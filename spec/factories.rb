b8568fcb42c5c02ab2c485f7f9e6bd1bd55bec3c
Vladislav Zhukovitskij <v.zhukovitskij@gmail.com>
2015-02-11 03:41:37 +0200

    Finishing user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Vlad Zhukovitskij"
+    email "alucarD@ua.fm"
+    password "test123"
+    password_confirmation "test123"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,18 +4,40 @@ describe "UserPages" do
 
   subject { page }
 
-  describe "signup page" do
-    before { visit signup_path}
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('')) }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 
-  # describe "GET /user_pages" do
-  #   it "works! (now write some real specs)" do
-  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get user_pages_index_path
-  #     response.status.should be(200)
-  #   end
-  # end
+  describe "signup page" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it 'should not create a user' do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "test123"
+        fill_in "Confirmation", with: "test123"
+      end
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+
+    #it { should have_content('Sign up') }
+    #it { should have_title(full_title('')) }
+  end
+
 end
